55d8a758306183beee12d633e20d75025b413bd5
dangakeri <dangakeri254@gmail.com>
2023-06-03 12:04:15 +0300

    sign up page

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
-  static const String routeName = '/';
+  static const String routeName = '/signin';
 
   @override
   State<SignInPage> createState() => _SignInPageState();
