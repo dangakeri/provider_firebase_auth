7b203f44597e7a157c3bdd4c347be0eab406bfde
dangakeri <dangakeri254@gmail.com>
2023-06-03 13:35:31 +0300

    injecting provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fb_auth/pages/home_page.dart';
+import 'package:fb_auth/pages/sign_in_page.dart';
+import 'package:fb_auth/pages/sign_up_page.dart';
+import 'package:fb_auth/pages/splash_page.dart';
+import 'package:fb_auth/provider/auth/auth_provider.dart';
+import 'package:fb_auth/repositories/auth_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fbAuth;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,16 +21,41 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Flutter auth'),
+    return MultiProvider(
+      providers: [
+        Provider<AuthRepository>(
+          create: (context) => AuthRepository(
+            firebaseFirestore: FirebaseFirestore.instance,
+            firebaseAuth: fbAuth.FirebaseAuth.instance,
+          ),
         ),
+        StreamProvider<fbAuth.User?>(
+          create: (context) => context.read<AuthRepository>().user,
+          initialData: null,
+        ),
+        ChangeNotifierProxyProvider(
+          create: (context) =>
+              AuthProvider(authRepository: context.read<AuthRepository>()),
+          update: (
+            BuildContext context,
+            fbAuth.User? userStream,
+            AuthProvider? authProvider,
+          ) =>
+              authProvider!..update(userStream),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: const SplashPage(),
+        routes: {
+          SignUpPage.routeName: (context) => const SignUpPage(),
+          SignInPage.routeName: (context) => const SignInPage(),
+          HomePage.routeName: (context) => const HomePage(),
+        },
       ),
     );
   }
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,10 @@
+import 'package:fb_auth/provider/auth/auth_provider.dart';
+import 'package:fb_auth/provider/auth/auth_state.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'home_page.dart';
+import 'sign_in_page.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -6,6 +12,16 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authState = context.watch<AuthProvider>().state;
+    if (authState.authStatus == AuthStatus.authenticated) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.pushNamed(context, HomePage.routeName);
+      });
+    } else if (authState.authStatus == AuthStatus.unauthenticated) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.pushNamed(context, SignInPage.routeName);
+      });
+    }
     return const Scaffold(
       body: Center(
         child: CircularProgressIndicator(),
